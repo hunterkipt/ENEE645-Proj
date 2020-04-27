8e512d53f68bb66f28acb4b6721653498b12ca8d
Hunter Kippen <hunterkipt@gmail.com>
2020-04-27 18:38:50 -0400

    Func_clone.cpp: Loop through inst

--- func_clone/lib/Func_clone/Func_clone.cpp
+++ func_clone/lib/Func_clone/Func_clone.cpp
@@ -1,25 +1,43 @@
 #include "llvm/Pass.h"
-#include "llvm/Module.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/ADT/ilist.h"
+#include "llvm/IR/Function.h" 
+#include "llvm/Support/Casting.h"
+#include "llvm/IR/Instructions.h"
 
-namespace {
-struct FuncClone :  public llvm::ModulePass
-{
+namespace   {
+    struct Proj : public llvm::ModulePass   {
+        static char ID;
+        Proj() : llvm::ModulePass(ID)   {}
 
-    static char ID;                           
-    FuncClone() : llvm::ModulePass(ID) {}
+        virtual bool runOnModule(llvm::Module &m)   {
+            llvm::ConstantInt* const_int32_7 = llvm::ConstantInt::get(m.getContext(), llvm::APInt(32, llvm::StringRef("5"), 10));
+            llvm::GlobalVariable* gvar_int32_global = new llvm::GlobalVariable(m, llvm::IntegerType::get(m.getContext(), 32), false,
+llvm::GlobalValue::ExternalLinkage, 0, "global");
 
-    virtual bool runOnModule(llvm::Module &M) {
-	llvm::errs() << "FuncClone: ";
-        llvm::errs().write_escaped(M.getName()) << "\n";
-        return false;
-    }
+            gvar_int32_global->setAlignment(4);
+            gvar_int32_global->setInitializer(const_int32_7);   
+            llvm::errs() << "Proj: ";
+            llvm::errs().write_escaped(m.getModuleIdentifier()) << '\n';
+            llvm::errs() << gvar_int32_global->getInitializer()->getUniqueInteger() << '\n';
+            for(llvm::Module::iterator Fb = m.begin(), Fe = m.end(); Fb != Fe; ++Fb)    {
+                llvm::Function *F = llvm::dyn_cast<llvm::Function>(&*Fb);
+                for (llvm::inst_iterator I = llvm::inst_begin(F), E = llvm::inst_end(F); I != E; ++I)   {
+                    if (llvm::CallInst* callInst = llvm::dyn_cast<llvm::CallInst>(&*I)) {
+                        llvm::errs() << *callInst << "\n";      
+                    }
+                }
+            }
+            llvm::errs() << gvar_int32_global->getInitializer()->getUniqueInteger() << '\n';
+            return false;
+        }
 
-};
+    };
 }
 
-char FuncClone::ID = 0;
-static RegisterPass<FuncClone> X("func_clone", "Function Cloning Pass", false, false);
-
-
-
+char Proj::ID = 0;
+static llvm::RegisterPass<Proj> X("proj", "Custom Pass", false, false);
